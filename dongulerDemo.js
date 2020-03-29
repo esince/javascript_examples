6036073092388b8c8b0240b6190715a072d9d50a
Esin Ece Aydın <esinece6@gmail.com>
2020-03-29 15:32:32 +0300

    loop added to tweet example
    tweet example that is written previously is updated by using a new function declaration. Inside of this function, user's information checks by using for loop and function returns boolean values.

---
+++ dongulerDemo.js
@@ -0,0 +1,39 @@
+//28.03.2020 Esin Ece Aydin
+
+var kullanicilar = [
+    {email:"aa@gmail.com", sifre:"1234"},
+    {email:"ee@gmail.com", sifre:"5678"}
+    
+]
+
+var tweetler = [
+    {email:"aa@gmail.com", tweet:"Bugün hava çok güzel."},
+    {email:"aa@gmail.com", tweet:"Bugün hava çok güzel 222."},
+    {email:"ee@gmail.com", tweet:"Kapıyı ben açacağım."}
+]
+
+var email = prompt("email: ")
+var sifre = prompt("şifre: ")
+
+
+function kullaniciVarMi(email, sifre){
+    for (let i = 0; i < kullanicilar.length; i++) {
+        if(email == kullanicilar[i].email && sifre == kullanicilar[i].sifre){
+            return true
+        }
+       
+    }
+    return false
+}
+
+function giris(){
+    kullaniciVarMi(email, sifre)
+    if(kullaniciVarMi(email, sifre)){
+        console.log(tweetler)
+    }
+    else{
+        console.log("Hatali giris!")
+    }
+}
+
+giris(email, sifre)
