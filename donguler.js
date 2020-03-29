448c6d6f4dfc66406ec2fa900d8b29b4d6070589
Esin Ece Aydın <esinece6@gmail.com>
2020-03-29 15:01:19 +0300

    loop examples
    for, while, do while, array.forEach() are learned

---
+++ donguler.js
@@ -0,0 +1,27 @@
+// for (let i = 1; i <= 10; i++) {
+//     console.log(i);
+    
+// }
+// console.log("finished")
+
+var sehirler = ["ankara", "bursa", "istanbul", "izmir"] 
+
+// for (let i = 0; i < sehirler.length; i++) {
+//     console.log(sehirler[i]);
+    
+// }
+
+// var i = 0
+// while (i < 10) {
+//     console.log(i)
+//     i++
+// }
+
+// var i = 2
+// do {
+//     console.log(i)
+//     i++
+// } while (i< 10);
+sehirler.forEach(function(sehir){
+    console.log(sehir)
+})
