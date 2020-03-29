e2f01340a6946ff708762ec8312684cae68d1424
Esin Ece Aydın <esinece6@gmail.com>
2020-03-29 15:58:54 +0300

    avoid error on dom example
    script is located under the body tag. So, we can avoid error.  files are updated

--- dom.js
+++ dom.js
@@ -1,6 +1,5 @@
 document.getElementById("bio").innerHTML="esc : 1998"
-
-
-
 //dom.js:1 Uncaught TypeError: Cannot set property 'innerHTML' 
 //of null at dom.js:1
+
+// error is handled by writting scipt in the body part of the html file
