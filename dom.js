965772fdd5d0718588fec817073102563a2d0e17
Esin Ece Aydın <esinece6@gmail.com>
2020-03-30 17:12:55 +0300

    class naming
    to prepare GetElementsByClassName(), intro1, intro2, intro3 are named to different classes. script file made empty.

--- dom.js
+++ dom.js
@@ -4,21 +4,23 @@
 
 // error is handled by writting scipt in the body part of the html file
 
-var intro1 = document.getElementById("intro1");
+// var intro1 = document.getElementById("intro1");
 
-var mesaj = document.getElementById("mesaj")
+// var mesaj = document.getElementById("mesaj")
 
-mesaj.innerHTML = intro1.innerHTML;
+// mesaj.innerHTML = intro1.innerHTML;
 
-var tumListeler = document.getElementsByTagName("ul")
+// var tumListeler = document.getElementsByTagName("ul")
 
-var sehirler = tumListeler[0]
+// var sehirler = tumListeler[0]
 
-tumElemanlar = sehirler.getElementsByTagName("li")
+// tumElemanlar = sehirler.getElementsByTagName("li")
 
-for (let i = 0; i < tumElemanlar.length; i++) {
-    alert(tumElemanlar[i].innerHTML)
-}
+// for (let i = 0; i < tumElemanlar.length; i++) {
+//     alert(tumElemanlar[i].innerHTML)
+// }
 
 
 // ctrl +k & ctrl +u --> uncomment
+
+
