906ac8fc5dc3e137e76e59e2f8b5cc95f711e15b
Esin Ece Aydın <esinece6@gmail.com>
2020-03-30 12:34:02 +0300

    GetElementsByTagName
    list elements are gotten by using GetElementsByTagName("li") and read information inside the tag by using .innerHTML

--- dom.js
+++ dom.js
@@ -1,5 +1,24 @@
-document.getElementById("bio").innerHTML="esc : 1998"
+// document.getElementById("bio").innerHTML="esc : 1998"
 //dom.js:1 Uncaught TypeError: Cannot set property 'innerHTML' 
 //of null at dom.js:1
 
 // error is handled by writting scipt in the body part of the html file
+
+var intro1 = document.getElementById("intro1");
+
+var mesaj = document.getElementById("mesaj")
+
+mesaj.innerHTML = intro1.innerHTML;
+
+var tumListeler = document.getElementsByTagName("ul")
+
+var sehirler = tumListeler[0]
+
+tumElemanlar = sehirler.getElementsByTagName("li")
+
+for (let i = 0; i < tumElemanlar.length; i++) {
+    alert(tumElemanlar[i].innerHTML)
+}
+
+
+// ctrl +k & ctrl +u --> uncomment
