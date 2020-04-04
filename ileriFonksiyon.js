e57bcbc1d3cba95436b002e5501011fc3cac319c
Esin Ece Aydın <esinece6@gmail.com>
2020-04-04 23:07:15 +0300

    arrow func without parameters

--- ileriFonksiyon.js
+++ ileriFonksiyon.js
@@ -1,5 +1,12 @@
 var selamFonksiyonu = function selam(){
-    console.log("Merhaba")
+    console.log("Merhaba 1")
 }
 
 selamFonksiyonu()
+
+const selamFonksiyonu2 = () =>{
+    console.log("Merhaba 2")
+    console.log("Hello 3")
+}
+
+selamFonksiyonu2()
