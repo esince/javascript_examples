41e97a4dd60273b129bed8955768c1bc20ec50ce
Esin Ece Aydın <esinece6@gmail.com>
2020-04-05 00:54:29 +0300

    constructor & string literal

--- class.js
+++ class.js
@@ -1,9 +1,21 @@
 class Personel{
+    constructor(ad,soyad){
+        this.ad = ad
+        this.soyad = soyad
+
+    }
     kaydet(){
-        console.log("Personel Kaydedildi")
+        // console.log("Personel Kaydedildi" + this.ad)
+        console.log(`Personel Kaydedildi: ${this.ad}`)
     }
 }
 
-const personel = new Personel()
+const personel = new Personel("Engin", "Demirog")
 
 personel.kaydet()
+console.log(personel.ad)
+
+
+//wrong if there is no constructor:
+// personel.ad = "engin"
+// console.log(personel.ad)
