6574b2f6b0bd10a3e98f09d16e3496d072c91ec9
Esin Ece Aydın <esinece6@gmail.com>
2020-04-04 23:43:51 +0300

    reduce & acc

--- ileriDizi.js
+++ ileriDizi.js
@@ -22,3 +22,10 @@ console.log(mapDizi)
 //filter
 const filtreliDizi = dizi.filter(sayi => sayi >= 2)
 console.log(filtreliDizi)
+
+//reduce
+const toplam = dizi.reduce((acc, sayi)=>{
+    return acc + sayi
+}, 10)
+
+console.log(toplam)
